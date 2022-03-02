021168bf04a9e521acff6fca8bb0188e9f8f301f
jess <jessica.elessa@gmail.com>
2022-03-02 05:46:11 +0100

    created visual interface and picked preselected example image

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  // Picking an image
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  };
+
+  // Using the selected image
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Image
+          source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+          style={styles.img}
+        />
+      </View>
+      <TouchableOpacity style={styles.btn} onPress={openImagePickerAsync}>
+        <Text style={styles.btnTxt}>Add a profile picture</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +36,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  circle: {
+    // width: 250,
+    // height: 250,
+    // borderRadius: 150,
+    backgroundColor: "white",
+    borderWidth: 1,
+  },
+  img: { width: 270, height: 270, borderRadius: 150 },
+  btn: {
+    backgroundColor: "#fff",
+    width: 200,
+    justifyContent: "center",
+    height: 50,
+    borderRadius: 8,
+    marginTop: 20,
+    borderWidth: 1,
+  },
+  btnTxt: {
+    fontSize: 20,
+    color: "black",
+    textAlign: "center",
   },
 });
